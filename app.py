b209e137b1eaa7fc4aacc2eec68491c2d13e9c35
hattajr <53370870+hattajr@users.noreply.github.com>
2024-06-24 02:14:47 +0900

    modify ui

--- app.py
+++ app.py
@@ -6,20 +6,19 @@ import polars as pl
 data = pl.read_csv("price.csv")
 print(data)
 items = st.multiselect(
-    "Beli apa",
+    "Barang/Items",
     data["items"].sort().to_list(),
     default=["Abc kopi"])
 
 
 total_cost=0
 for ix,i in enumerate(items):
-    # with st.container(height=150):
-    amount_col, total_col = st.columns(2, vertical_alignment="center")
-    price = data.filter(pl.col("items") == i).item(0,"price") 
-    amount = amount_col.number_input(f"{i} `₩{price}`", value=1, key=i ,min_value=1)
-    total_cost_ = price * amount 
-    total_col.write(total_cost_)
-    total_cost += total_cost_
+    with st.container(height=150):
+        price = data.filter(pl.col("items") == i).item(0,"price") 
+        amount = st.number_input(f"{i} `₩{price}`", value=1, key=i ,min_value=1)
+        total_cost_ = price * amount 
+        st.write(total_cost_)
+        total_cost += total_cost_
 
 st.subheader(f"**Total: `₩{total_cost}`**")
 
