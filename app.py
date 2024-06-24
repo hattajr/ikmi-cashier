efc93733970656fe7b1ed425339b76e15782bf78
hattajr <53370870+hattajr@users.noreply.github.com>
2024-06-24 16:04:41 +0900

    add image item

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 import polars as pl
 from streamlit_tags import st_tags
 
@@ -23,7 +24,11 @@ total_cost=0
 for ix,i in enumerate(items):
     with st.container(height=230):
         price = data.filter(pl.col("items") == i).item(0,"price") 
-        st.image(f"images/{i}.png")
+        image_path = f"images/{i}.png"
+        if os.path.exists(image_path):
+            st.image(image_path, width=100)
+        else: 
+            st.image("images/no_image.jpg", width=100)
         amount = st.number_input(f"{i} `₩{price:,}`", value=1, key=i ,min_value=1)
         total_cost_ = price * amount 
         st.markdown(f"`₩{price:,} x {amount} = ₩{total_cost_:,}`")
