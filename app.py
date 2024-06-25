c1dc1eb854a033ac70c3585d0ef00902780f60e9
hattajr <atadonjep@gmail.com>
2024-06-25 19:02:47 +0900

    add result detail

--- app.py
+++ app.py
@@ -58,6 +58,7 @@ items = st.multiselect(
 
 
 total_cost = 0
+total_item = 0
 for ix, i in enumerate(items):
     with st.container(height=250):
         price = data.filter(pl.col("Produk") == i).item(0, "Harga")
@@ -67,8 +68,12 @@ for ix, i in enumerate(items):
         else:
             st.image("images/no_image.jpg", width=100)
         amount = st.number_input(f"{i} `₩{price}`", value=1, key=i, min_value=1)
+        total_item += amount
         total_cost_ = price * amount
         st.markdown(f"`₩{price} x {amount} = ₩{total_cost_}`")
         total_cost += total_cost_
 
-st.info(f"**Total(₩): {total_cost:,}**")
+st.success(f"""
+    **Jumlah Barang : {total_item:,}**\n
+    **Total Harga (₩): {total_cost:,}**
+""")
