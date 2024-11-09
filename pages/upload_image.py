0b5bc945a9826280f35ce16b25c5806544908e06
hattajr <53370870+hattajr@users.noreply.github.com>
2024-11-09 22:53:51 +0900

    uploader ready

--- pages/upload_image.py
+++ pages/upload_image.py
@@ -1,21 +1,20 @@
-import streamlit as st
-import polars as pl
 import os
 import time
-from streamlit import caching
 
-caching.clear_cache()
+import polars as pl
+import streamlit as st
 
 st.set_page_config(layout="centered", initial_sidebar_state="collapsed")
 
 PRICE_FILEPATH = "price_test.csv"
 
 
-
 if os.path.exists(PRICE_FILEPATH):
     data = pl.read_csv(PRICE_FILEPATH)
     data = data.with_columns(pl.col("Harga").cast(pl.Int64))
-    produk = st.selectbox("Pilih nama produk", data["Produk"].to_list(), index=None, key="produk")
+    produk = st.selectbox(
+        "Pilih nama produk", data["Produk"].to_list(), index=None, key="produk"
+    )
     st.write("`If you can not find the product name, please update it in GSheet`")
 
 if produk:
@@ -23,9 +22,8 @@ if produk:
 
 if produk and picture:
     if st.button("Submit"):
-        with open (f'images/{produk}.jpg','wb') as file:
+        with open(f"images/{produk}.jpg", "wb") as file:
             file.write(picture.getbuffer())
         st.success("Gambar berhasil disimpan :)")
         time.sleep(0.3)
         st.rerun()
-
